e68390d10b98e95d7a5565b9591ab75f84f06aae
julia <juliamatsievich@mail.ru>
2024-11-21 23:55:06 +0500

    body

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { SoundNotification } from './notifications.types';
 
@@ -7,7 +7,7 @@ export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
   @Post('send')
-  async sendNotification(@Body('message') title: string, sound: SoundNotification, body: string) {
+  async sendNotification(title: string, sound: SoundNotification, body: string) {
     return await this.notificationsService.sendNotification(title, sound, body);
   }
 }
